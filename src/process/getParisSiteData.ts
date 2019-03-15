93cef2f4d00ffe715a5ea86e52faa2ad3643fb1c
Rodrigo  Hinojosa <rodrigo.hinojosa.developer@gmail.com>
2019-03-14 23:54:30 -0300

    WIP

--- src/process/getParisSiteData.ts
+++ src/process/getParisSiteData.ts
@@ -1,19 +1,19 @@
 import * as cheerio from 'cheerio';
 import * as request from 'request-promise';
 
-import {Category} from './models/Category';
-import {Product} from './models/Product';
-import {SubCategory} from './models/SubCategory';
+import {Category} from '../models/Category';
+import {Product} from '../models/Product';
+import {SubCategory} from '../models/SubCategory';
 
-import {categoryList} from './data-for-scraping/categoryList';
-import {Utils} from './utils';
+import {categoryList} from '../data-for-scraping/categoryList';
+import {Utils} from '../utils';
 
 const BASE_URL: string = 'https://www.paris.cl';
 
-export const ParisSiteScrapping = {
+export const getParisSiteData = {
 
     initialize: async (): Promise<void> => {
-        const scrapingData = await ParisSiteScrapping.getCategories(categoryList);
+        const scrapingData = await getParisSiteData.getCategories(categoryList);
 
         Utils.writeJsonFile(scrapingData);
     },
@@ -21,7 +21,7 @@ export const ParisSiteScrapping = {
 
         for (const category of categories) {
 
-            category.subCategories = await ParisSiteScrapping.getSubCategories(category.subCategories);
+            category.subCategories = await getParisSiteData.getSubCategories(category.subCategories);
 
         }
 
@@ -52,7 +52,7 @@ export const ParisSiteScrapping = {
 
             });
 
-            subCategory.products = await ParisSiteScrapping.getProducts(productsHref);
+            subCategory.products = await getParisSiteData.getProducts(productsHref);
 
             $PLP('div.col-md-6.col-sm-12.border-right.center.pages.pages-top.d-md-none > div > a').each((index, page) => {
 
@@ -73,7 +73,7 @@ export const ParisSiteScrapping = {
 
         for (const productHref of productsHref) {
 
-            await new Promise((resolve) => setTimeout(resolve, 1500));
+            // await new Promise((resolve) => setTimeout(resolve, 1500));
 
             const url = BASE_URL.concat(productHref);
 
@@ -92,7 +92,11 @@ export const ParisSiteScrapping = {
             const shortDescription = $PDP('p.short-description').text().trim();
             const internetPrice = parseInt($PDP('#product-content > div.price > div.item-price').text().trim().replace(/\D/g, ''), 0);
             const normalPrice = parseInt($PDP('#product-content > div.price > div.row-price > div.column-price.details-price > div > s').text().trim().replace(/\D/g, ''), 0);
+            const ProductDetails = $PDP('#collapseDetails > div > table > tbody > tr') ? $PDP('#collapseDetails > div > table > tbody > tr') : null;
+            const brand = getParisSiteData.getProductsDetail.brand(ProductDetails, $PDP);
             const img = $PDP('div.box-foto.product-primary-image.slider-zoom > div > a').attr('href');
+            const pageTitle = $PDP('title').text();
+            const pageDescription = $PDP('meta[name=description]').attr('content');
 
             const product = new Product(
                 sku,
@@ -101,7 +105,10 @@ export const ParisSiteScrapping = {
                 shortDescription,
                 internetPrice,
                 normalPrice,
-                img
+                brand,
+                img,
+                pageTitle,
+                pageDescription
             );
 
             products.push(product);
@@ -110,6 +117,40 @@ export const ParisSiteScrapping = {
 
         return products;
 
+    },
+    getProductsDetail: {
+
+        brand: (ProductDetail, $PDP): string => {
+
+            let brand = '';
+
+            if (ProductDetail) {
+
+                ProductDetail.each((index, row) => {
+
+                    $PDP(row.children).each((i, col) => {
+
+                        if ($PDP(col).text() === 'Marca') {
+
+                            brand = $PDP(row.children[i + 2]).text();
+
+                            return;
+
+                        }
+
+                    });
+
+                    if (brand) {
+                        return;
+                    }
+
+                });
+
+            }
+
+            return brand;
+
+        }
     }
 
 };
---
+++ src/process/jsonDataToXml.ts
@@ -0,0 +1,4 @@
+import * as request from 'request-promise';
+
+const jsonToXML = {
+};
